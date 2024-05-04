3c876c256560f523cb2e4c7e0ac1c9583487bfb9
abhis12321 <abhisheksingh2359328@gmail.com>
2024-05-05 04:21:13 +0530

    Dark/Light Mode enabled in Home-page

--- src/app/page.js
+++ src/app/page.js
@@ -19,7 +19,7 @@ import Writer from './animated_typewritter';
 export default function page() {
   return (
     <div className="">
-      <div className="flex flex-col-reverse md:flex-row items-center justify-center bg-gradient-to-b dark:bg-gradient-to-r from-teal-950/80 via-sky-950/90 to-sky-950 dark:from-cyan-950/70 dark:via-slate-900/80 dark:to-gray-950 text-white bg-[ul('/Ab2.png')] bg-no-repeat bg-center py-2 border-b-2 border-gray-800" style={{ minHeight: "calc(100vh - 4rem)" }}>
+      <div className="flex flex-col-reverse md:flex-row items-center justify-center bg-gradient-to-br dark:bg-gradient-to-r from-teal-950/80 via-sky-950/90 to-sky-950 dark:from-cyan-950/70 dark:via-slate-900/80 dark:to-gray-950 text-white bg-[ul('/Ab2.png')] bg-no-repeat bg-center py-2 border-b-2 border-gray-800" style={{ minHeight: "calc(100vh - 4rem)" }}>
         <div className="flex flex-col gap-4 max-w-[95%] md:w-[66%] md:pl-[9vw] lg:pl-0 lg:w-[45%] mx-auto items-center md:items-start">
           <h3 className="text-justify drop-shadow-[0_0_10px_red] text-xl md:text-2xl md:font-semibold">
             👋🏻 Hi there, I&apos;m
@@ -71,10 +71,10 @@ export default function page() {
         <AnimatedPhoto />
       </div>
 
-      <div className='flex flex-col items-center justify-evenly bg-gradient-to-b from-gray-950 via-slate-950 to-slate-900 py-8 border-b-2 border-slate-800' id="about">
-        <div className="rounded-2xl w-full xs:w-[95%] flex flex-col items-center justify-evenly max-w-[600px] ">
+      <div className='flex flex-col items-center justify-evenly py-8 border-b-2 border-slate-800 bg-gray-400 dark:bg-slate-950' id="about">
+        <div className="rounded-2xl w-[98%] max-w-[650px] flex flex-col items-center justify-evenly bg-slate-700 dark:bg-gradient-to-b dark:from-gray-950 dark:via-cyan-950 dark:to-slate-900 shadow-[0_0_5px_white] py-4 px-3 xs:px-6 sm:px-12">
           <h1 className='text-center py-4 px-8 mb-2 rounded-md drop-shadow-[0_0_1px_white] text-4xl sm:text-5xl font-extrabold sm:font-bold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-violet-900 w-fit'>About me</h1>
-          <div className="text-center px-6 sm:px-12 text-gray-400 font-mono">I am a passionate web developer and  
+          <div className="text-center text-gray-400 font-mono text-[14px] xs:text-[16px]">I am a passionate web developer and  
           <span className="font-bold text-red-900/95"> currently pursuing my BTech(Computer Science and Engineering) from Dr. Ambedkar Institute of Technology for Handicapped Kanpur</span>, I&apos;m skilled in front-end
             technologies like HTML5, CSS3, JavaScript, Bootstrap, Tailwind and React.js as well as back-end frameworks like Node.js ,
             Express.js, Next.js. Also Skilled in responsive design principles and eager to contribute innovative solutions to web
@@ -84,21 +84,21 @@ export default function page() {
         </div>
       </div>
 
-      <section className="gap-6 bg-gray-950  text-white py-5 flex items-center justify-center flex-col " id="learnings">
+      <section className="gap-6 bg-slate-400 dark:bg-transparent text-white py-5 flex items-center justify-center flex-col" id="learnings">
         <h1 className="mx-auto text-center py-4 sm:px-8 my-2 text-4xl rounded-md drop-shadow-[0_0_1px_white] font-extrabold  text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-violet-800 w-[95%] xs:w-fit">
           Coding Journey
         </h1>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center py-4 xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faC}
               size="8x"
-              className="xs:bg-blue-950 text-gray-300 xs:px-4 py-2 md:px-5 md:py-3 rounded-lg h-[6rem]"
+              className="xs:bg-blue-950 text-gray-300 px-1 xs:px-4 py-2 md:px-5 md:py-3 rounded-lg h-[6rem]"
             />
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               February 2022
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -110,17 +110,17 @@ export default function page() {
         </div>
 
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center py-4 xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faJava}
               size="8x"
-              className="xs:bg-orange-800 text-gray-300 xs:px-4 py-2 rounded-lg h-[6.2rem] md:h-[6rem]"
+              className="xs:bg-orange-800 text-gray-300 xs:px-4 py-2 rounded-lg h-[6.2rem] md:h-[6rem] drop-shadow-[0_0_1px_black]"
             />
             <h3 className="font-bold ">Java</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               August 2022
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -131,17 +131,17 @@ export default function page() {
           </div>
         </div>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faHtml5}
               size="8x"
-              className="text-orange-700 py-2 rounded-lg h-[6.4rem] md:h-[6.9rem]"
+              className="text-orange-800 py-2 rounded-lg h-[6.4rem] md:h-[6.9rem] drop-shadow-[0_0_1px_black]"
             />
             <h3 className="font-bold ">HTML</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               December 2022
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -151,17 +151,17 @@ export default function page() {
           </div>
         </div>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faCss3Alt}
               size="8x"
-              className="text-blue-800 h-[6.7rem] md:h-[6.9rem]"
+              className="text-blue-900 h-[6.7rem] md:h-[6.9rem] drop-shadow-[0_0_1px_black]"
             />
             <h3 className="font-bold ">CSS</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               December 2022
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -172,17 +172,17 @@ export default function page() {
           </div>
         </div>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faSquareJs}
               size="8x"
-              className="text-yellow-700 h-[6.1rem] md:h-[6.7rem]"
+              className="text-yellow-700 h-[6.1rem] md:h-[6.7rem] drop-shadow-[0_0_1px_black]"
             />
             <h3 className="font-bold ">JavaScript</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               February 2023
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -193,17 +193,17 @@ export default function page() {
           </div>
         </div>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faBootstrap}
               size="8x"
-              className="text-violet-700 h-[5rem] md:h-[6.7rem]"
+              className="text-violet-800 h-[5rem] md:h-[6.7rem] drop-shadow-[0_0_1px_black]"
             />
             <h3 className="font-bold ">Bootstrap</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               April 2023
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -215,17 +215,17 @@ export default function page() {
           </div>
         </div>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faGithub}
               size="8x"
-              className="text-gray-300 h-[5.7rem] md:h-[6.7rem]"
+              className="text-gray-950 dark:text-gray-300 h-[5.7rem] md:h-[6.7rem]"
             />
             <h3 className="font-bold ">Git & Github</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               May 2023
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -234,7 +234,7 @@ export default function page() {
           </div>
         </div>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg p-1 px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 p-1 px-4 ">
           <div className="flex flex-col justify-center items-center xs:w-[30%] ">
             <FontAwesomeIcon
               icon={faReact}
@@ -244,7 +244,7 @@ export default function page() {
             <h3 className="font-bold ">ReactJS</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               June 2023
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
@@ -255,8 +255,8 @@ export default function page() {
           </div>
         </div>
 
-        <div className="min-h-[40vh] flex gap-3 w-[95%] md:w-[80%] lg:max-w-[65%] rounded-lg px-4 shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-gray-900 ">
-          <div className="flex flex-col justify-center items-center py-4 xs:w-[30%] ">
+        <div className="min-h-[40vh] flex gap-3 w-[98%] max-w-[850px] rounded-lg shadow-[0_0_10px_white] hover:shadow-[0_0_15px_red] bg-slate-700 dark:bg-gray-900 px-3 xs:px-4">
+          <div className="flex flex-col justify-center items-center py-4 xs:w-[30%]">
             <FontAwesomeIcon
               icon={faN}
               size="5x"
@@ -265,7 +265,7 @@ export default function page() {
             <h3 className="font-bold ">NextJS</h3>
           </div>
           <div className="flex flex-col justify-center items-center py-4 xs:w-[70%]">
-            <h2 className="bg-gray-950 font-bold px-4 py-2 rounded">
+            <h2 className="bg-gray-950/50 dark:bg-gray-950 font-bold px-4 py-2 rounded">
               September 2023
             </h2>
             <p className="font-mono text-[14px] text-gray-400 py-3">
