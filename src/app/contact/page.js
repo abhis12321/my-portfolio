a8faa477a0c9cb4949dc4210d5339b013f6af6b1
abhis12321 <abhisheksingh2359328@gmail.com>
2024-05-04 12:08:11 +0530

    Toggle Mode enabled in Contact Page for dark/light mode

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -17,13 +17,13 @@ export default function Page() {
 
   return (
     <div className='flex justify-center items-center py-3' style={{minHeight:'calc(100vh - 4rem)'}} >
-      <form className="flex flex-col gap-2 bg-gradient-to-tr from-slate-950 via-cyan-950 to-slate-900 bg-slate-800 w-[98%] max-w-[700px] px-2 py-5 sm:p-8 rounded-lg shadow-[0_0_5px_gray]" onSubmit={handleSubmit}>
-        <h1 className='mx-auto text-center py-4 sm:px-8 text-4xl rounded-md font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-500 to-violet-500 w-fit'>Contact me</h1>
+      <form className="flex flex-col gap-2 text-white bg-gradient-to-tr from-gray-950/60 via-teal-800/40 to-gray-950/60 dark:from-slate-950 dark:via-cyan-950 dark:to-slate-900 w-[98%] max-w-[700px] px-2 py-5 sm:p-8 rounded-lg shadow-[0_0_5px_gray] hover:shadow-[0_0_8px_indigo] dark:hover:shadow-[0_0_8px_lime]" onSubmit={handleSubmit}>
+        <h1 className='mx-auto text-center py-4 sm:px-8 text-4xl rounded-md font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-pink-600 to-violet-600 w-fit font-sans'>Contact me</h1>
       
         <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder='example@gmail.com' className='bg-slate-900/40 focus:border-cyan-700 border-2 border-gray-700 p-2 text-center outline-none rounded' required/>
         <input type="text" value={subject} onChange={e => setSubject(e.target.value)} placeholder='Enter your subject/topic' className='bg-slate-900/40 focus:border-cyan-700 border-2 border-gray-700 p-2 text-center outline-none rounded' required/>
         <textarea value={message} onChange={e => setMessage(e.target.value)} placeholder='Enter your message/suggestion' cols="30" rows="10" className='bg-slate-900/40 focus:border-cyan-700 border-2 border-gray-700 p-2 text-center outline-none rounded' required></textarea>
-        <input type="submit" value={'Send'} placeholder='Enter your subject/topic' className='bg-red-950 hover:bg-red-700 active:bg-violet-900 border-2 border-gray-700 p-2 text-center outline-none rounded font-bold cursor-pointer' required/>
+        <input type="submit" value={'Send'} placeholder='Enter your subject/topic' className='bg-red-900 hover:bg-red-700 active:bg-violet-900 border-2 border-gray-700 p-2 text-center outline-none rounded font-bold cursor-pointer' required/>
       </form>
     </div>
   )
