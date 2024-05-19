556fbc6d5f255cfef90431d7f00fb3ddb6662114
abhis12321 <abhisheksingh2359328@gmail.com>
2024-05-20 00:29:06 +0530

    Contact page is modified with input data and UI

--- src/app/_components/Contact.js
+++ src/app/_components/Contact.js
@@ -8,16 +8,16 @@ import Image from 'next/image';
 
 export default function Contact() {
   const [email, setEmail] = useState('');
-  const [subject, setSubject] = useState('');
+  const [name, setName] = useState('');
   const [message, setMessage] = useState('');
 
   const handleSubmit = e => {
     e.preventDefault();
-    console.log(email, subject, message);
+    console.log(email, name, message);
 
 
     setEmail('');
-    setSubject('');
+    setName('');
     setMessage('');
   }
 
@@ -38,14 +38,14 @@ export default function Contact() {
         <div className="flex flex-col gap-[2px] xs:gap-1 w-[100%] sm:w-[60%] sm:max-w-[500px]">
           <div className="group w-full flex relative">
             <FontAwesomeIcon size='1x' className='absolute top-[16px] left-5 text-xl group-focus-within:text-violet-700 text-slate-950 dark:text-gray-100' icon={faUser} />
+            <input type="text" value={name} onChange={e => setName(e.target.value)} placeholder='Enter your name' className='w-full bg-blue-950/15 dark:bg-slate-950/45 text-cyan-950 dark:text-gray-400 placeholder:text-gray-500 dark:placeholder:text-gray-600 font-medium focus:text-sky-700 focus:bg-blue-950/40 dark:focus:bg-slate-950/60 focus:border-violet-700 dark:focus:border-violet-900 border-2 border-gray-500 p-3 px-14 outline-none rounded-md' required />
+          </div>
+          <div className="group w-full flex relative">
+            <FontAwesomeIcon size='1x' className='absolute top-[16px] left-5 text-xl group-focus-within:text-violet-700 text-slate-950 dark:text-gray-100' icon={faEnvelope} />
             <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder='example@gmail.com' className='w-full bg-blue-950/15 dark:bg-slate-950/45 text-cyan-950 dark:text-gray-400 placeholder:text-gray-500 dark:placeholder:text-gray-600 font-medium focus:text-sky-700 focus:bg-blue-950/40 dark:focus:bg-slate-950/60 focus:border-violet-700 dark:focus:border-violet-900 border-2 border-gray-500 p-3 px-14 outline-none rounded-md' required />
           </div>
           <div className="group w-full flex relative">
             <FontAwesomeIcon size='1x' className='absolute top-[16px] left-5 text-xl group-focus-within:text-violet-700 text-slate-950 dark:text-gray-100' icon={faSignature} />
-            <input type="text" value={subject} onChange={e => setSubject(e.target.value)} placeholder='Enter your subject/topic' className='w-full bg-blue-950/15 dark:bg-slate-950/45 text-cyan-950 dark:text-gray-400 placeholder:text-gray-500 dark:placeholder:text-gray-600 font-medium focus:text-sky-700 focus:bg-blue-950/40 dark:focus:bg-slate-950/60 focus:border-violet-700 dark:focus:border-violet-900 border-2 border-gray-500 p-3 px-14 outline-none rounded-md' required />
-          </div>
-          <div className="group w-full flex relative">
-            <FontAwesomeIcon size='1x' className='absolute top-[16px] left-5 text-xl group-focus-within:text-violet-700 text-slate-950 dark:text-gray-100' icon={faEnvelope} />
             <textarea value={message} onChange={e => setMessage(e.target.value)} placeholder='Enter your message' cols="25" rows="10" className='w-full bg-blue-950/15 dark:bg-slate-950/45 text-cyan-950 dark:text-gray-400 placeholder:text-gray-500 dark:placeholder:text-gray-600 font-medium focus:text-sky-700 focus:bg-blue-950/40 dark:focus:bg-slate-950/60 focus:border-violet-700 dark:focus:border-violet-900 border-2 border-gray-500 p-3 px-14 outline-none rounded-md' required></textarea></div>
           <input type="submit" value={'Send'} className='bg-red-900 hover:bg-red-700 active:bg-violet-900 border-2 border-gray-500 py-[10px] text-center outline-none rounded-md font-bold cursor-pointer' required />
 
